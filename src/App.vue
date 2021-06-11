84a765666f49296d3c4300ce736fbd7872f491ae
iqbalmnaufal <iqbalmnaufal@gmail.com>
2021-06-11 10:01:20 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container" style="margin-top: 20px;">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">SIMPLE TODO APP</h5>
+        <div class="row">
+          <div class="col-10">
+            <input v-model="todo" type="text" class="form-control">
+          </div>
+          <div class="col-2">
+            <button class="btn btn-success" @click="add">ADD</button>
+          </div>
+        </div>
+        <List :todos="todos" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import List from './components/List.vue'
+export default {
+  components: {List},
+  data(){
+    return{
+      todo: "",
+      todos: []
+    }
+  },
+  methods: {
+    add(){
+      this.todos.push(this.todo)
+    }
+  }
+}
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="margin-top: 20px;">
+    <ul class="list-group">
+      <li class="list-group-item" v-for="todo in todos" :key="todo">{{ todo }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
